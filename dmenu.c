19dcce7095d4bb8676cba2dd52bcbdd44c9860a2
philor1 <scheibe.philipp@gmail.com>
2021-04-14 13:48:33 +0200

    add highlight support for mouse-patch

--- dmenu.c
+++ dmenu.c
@@ -600,8 +600,10 @@ buttonpress(XEvent *e)
 	}
 	if (ev->button != Button1)
 		return;
+//	this is commented out in the original highlight-mouse-patch...
 	if (ev->state & ~ControlMask)
 		return;
+//	...end
 	if (lines > 0) {
 		/* vertical list: (ctrl)left-click on item */
 		w = mw - x;
@@ -661,6 +663,57 @@ buttonpress(XEvent *e)
 			calcoffsets();
 			drawmenu();
 			return;
+		}
+	}
+}
+
+static void
+mousemove(XEvent *e)
+{
+	struct item *item;
+	XPointerMovedEvent *ev = &e->xmotion;
+	int x = 0, y = 0, h = bh, w, item_num = 0;
+
+	if (lines > 0 && columns > 0) {
+		w = mw - x;
+		for (item = curr; item != next; item = item->right) {
+			if (item_num++ == lines){
+				item_num = 1;
+				x += w / columns;
+				y = 0;
+			}
+			y += h;
+			if (ev->y >= y && ev->y <= (y + h) &&
+			    ev->x >= x && ev->x <= (x + w / columns)) {
+				sel = item;
+				calcoffsets();
+				drawmenu();
+				return;
+			}
+		}
+	} else if (lines > 0) {
+		w = mw - x;
+		for (item = curr; item != next; item = item->right) {
+			y += h;
+			if (ev->y >= y && ev->y <= (y + h)) {
+				sel = item;
+				calcoffsets();
+				drawmenu();
+				return;
+			}
+		}
+	} else if (matches) {
+		x += inputw;
+		w = TEXTW("<");
+		for (item = curr; item != next; item = item->right) {
+			x += w;
+			w = MIN(TEXTW(item->text), mw - x - TEXTW(">"));
+			if (ev->x >= x && ev->x <= x + w) {
+				sel = item;
+				calcoffsets();
+				drawmenu();
+				return;
+			}
 		}
 	}
 }
@@ -728,6 +781,9 @@ run(void)
 			exit(1);
 		case ButtonPress:
 			buttonpress(&ev);
+			break;
+		case MotionNotify:
+			mousemove(&ev);
 			break;
 		case Expose:
 			if (ev.xexpose.count == 0)
@@ -827,7 +883,7 @@ setup(void)
 	swa.override_redirect = True;
 	swa.background_pixel = scheme[SchemeNorm][ColBg].pixel;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask |
-	                 ButtonPressMask;
+	                 ButtonPressMask | PointerMotionMask;
 	win = XCreateWindow(dpy, parentwin, x, y, mw, mh, 0,
 	                    CopyFromParent, CopyFromParent, CopyFromParent,
 	                    CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
