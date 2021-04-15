bc72bcdee2b68b6b06e8199e7f79d5e59b7031df
philor1 <scheibe.philipp@gmail.com>
2021-04-15 21:16:00 +0200

    dynamicoptions-patch on vanilla-dmenu (bakkeby--no conflict with stdin-patch...)

--- dmenu.c
+++ dmenu.c
@@ -58,6 +58,8 @@ static Clr *scheme[SchemeLast];
 static int (*fstrncmp)(const char *, const char *, size_t) = strncmp;
 static char *(*fstrstr)(const char *, const char *) = strstr;
 
+static void refreshoptions();
+
 static void
 appenditem(struct item *item, struct item **list, struct item **last)
 {
@@ -213,6 +215,9 @@ grabkeyboard(void)
 static void
 match(void)
 {
+	if (dynamic && *dynamic)
+		refreshoptions();
+
 	static char **tokv = NULL;
 	static int tokn = 0;
 
@@ -234,7 +239,7 @@ match(void)
 		for (i = 0; i < tokc; i++)
 			if (!fstrstr(item->text, tokv[i]))
 				break;
-		if (i != tokc) /* not all tokens match */
+		if (i != tokc && !(dynamic && *dynamic)) /* not all tokens match */
 			continue;
 		/* exact matches go first, then prefixes, then substrings */
 		if (!tokc || !fstrncmp(text, item->text, textsize))
@@ -548,6 +553,55 @@ readstdin(void)
 }
 
 static void
+readstream(FILE* stream)
+{
+	char buf[sizeof text], *p;
+	size_t i, imax = 0, size = 0;
+	unsigned int tmpmax = 0;
+
+	/* read each line from stdin and add it to the item list */
+	for (i = 0; fgets(buf, sizeof buf, stream); i++) {
+		if (i + 1 >= size / sizeof *items)
+			if (!(items = realloc(items, (size += BUFSIZ))))
+				die("cannot realloc %u bytes:", size);
+		if ((p = strchr(buf, '\n')))
+			*p = '\0';
+		if (!(items[i].text = strdup(buf)))
+			die("cannot strdup %u bytes:", strlen(buf) + 1);
+		items[i].out = 0;
+		drw_font_getexts(drw->fonts, buf, strlen(buf), &tmpmax, NULL);
+		if (tmpmax > inputw) {
+			inputw = tmpmax;
+			imax = i;
+		}
+	}
+	if (items)
+		items[i].text = NULL;
+	inputw = items ? TEXTW(items[imax].text) : 0;
+	if (!dynamic || !*dynamic)
+		lines = MIN(lines, i);
+} 
+
+static void
+refreshoptions()
+{
+	int dynlen = strlen(dynamic);
+	char* cmd= malloc(dynlen + strlen(text) + 2);
+	if (cmd == NULL)
+		die("malloc:");
+	sprintf(cmd, "%s %s", dynamic, text);
+	FILE *stream = popen(cmd, "r");
+	if (!stream)
+		die("popen(%s):", cmd);
+	readstream(stream);
+	int pc = pclose(stream);
+	if (pc == -1)
+		die("pclose:");
+	free(cmd);
+	curr = sel = items;
+}
+
+static void
 run(void)
 {
 	XEvent ev;
@@ -690,7 +744,7 @@ static void
 usage(void)
 {
 	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
-	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
+	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid] [ -dy command]\n", stderr);
 	exit(1);
 }
 
@@ -733,6 +787,8 @@ main(int argc, char *argv[])
 			colors[SchemeSel][ColFg] = argv[++i];
 		else if (!strcmp(argv[i], "-w"))   /* embedding window id */
 			embed = argv[++i];
+		else if (!strcmp(argv[i], "-dy"))  /* dynamic command to run */
+			dynamic = argv[++i];
 		else
 			usage();
 
@@ -759,9 +815,11 @@ main(int argc, char *argv[])
 
 	if (fast && !isatty(0)) {
 		grabkeyboard();
-		readstdin();
+		if (!(dynamic && *dynamic))
+			readstdin();
 	} else {
-		readstdin();
+		if (!(dynamic && *dynamic))
+			readstdin();
 		grabkeyboard();
 	}
 	setup();
