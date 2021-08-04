af7517fc65ae15a8e2e3ed00ace07741cef697c9
philor1 <scheibe.philipp@gmail.com>
2021-08-04 23:42:00 +0200

    patch-fuzzymatch on vanilla-dmenu

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int opacity = 1;                     /* -o  option; if 0, then alpha is disabled */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
